1e8a0bc2b3de741a75767da26a4327a5ce848699
Shane Weisz <shaneweisz@gmail.com>
2022-02-14 17:09:20 +0000

    Put STO normalisation in first experiment

--- experiments/onebest-kws.py
+++ experiments/onebest-kws.py
@@ -1,5 +1,6 @@
 from constants.paths import OUTPUT_PATH, RESULTS_PATH
 from helpers.score_system import MTWVs, get_MTWVs_for_output_file
+from set_of_hits import SetOfHits
 
 experiment_name = "onebest-kws"
 
@@ -21,7 +22,27 @@ def main():
         line = get_results_line(mtwvs, output_file_name)
         write_line_to_file(results_file, line)
 
+        normalised_output_file_name = normalise(output_file_name, gamma=1)
+        mtwvs = get_MTWVs_for_output_file(OUTPUT_PATH / normalised_output_file_name)
+        line = get_results_line(mtwvs, normalised_output_file_name)
+        write_line_to_file(results_file, line)
+
     cleanup_results_file(results_file)
+
+
+def normalise(output_file_name, gamma):
+    set_of_hits = SetOfHits.from_XML(OUTPUT_PATH / output_file_name)
+    set_of_hits.normalise_scores(gamma)
+
+    normalised_output_file_name = get_normalised_file_name(output_file_name, gamma)
+    normalised_output_file_path = OUTPUT_PATH / normalised_output_file_name
+    set_of_hits.write_hits_to_file(normalised_output_file_path)
+
+    return normalised_output_file_name
+
+
+def get_normalised_file_name(output_file_name, gamma):
+    return output_file_name.strip(".xml") + "-STO.xml"
 
 
 def setup_results_file(experiment_name: str):
